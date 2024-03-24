7700a7c002b87468a2d64674344047cd67e14559
sahaquielxx <pankihoyy@icloud.com>
2024-03-25 02:54:05 +0300

    UserDaoJDBCImpl -

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,100 +1,35 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    private final Connection conn = Util.getConnection();
-
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-        String sql = "CREATE TABLE IF NOT EXISTS users(" +
-                "ID BIGINT NOT NULL AUTO_INCREMENT, NAME VARCHAR(100), " +
-                "LASTNAME VARCHAR(100), AGE INT, PRIMARY KEY (ID))";
-        try (PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
-            preparedStatement.executeUpdate(sql);
-            System.out.println("Table was created!");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+
     }
 
     public void dropUsersTable() {
-        String sql = "DROP TABLE IF EXISTS users";
 
-        try (PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
-            preparedStatement.executeUpdate(sql);
-            System.out.println("Table was dropped");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        String sql = "INSERT INTO users (NAME, LASTNAME, AGE) VALUES(?, ?, ?)";
 
-        try (PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, lastName);
-            preparedStatement.setByte(3, age);
-            preparedStatement.executeUpdate();
-            System.out.println("User " + name + " was added!");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
     public void removeUserById(long id) {
-        String sql = "DELETE FROM users WHERE ID=?";
 
-        try (PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
-            preparedStatement.executeUpdate(sql);
-            System.out.println("User was removed!");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
     public List<User> getAllUsers() {
-        List<User> userList = new ArrayList<>();
-
-        String sql = "SELECT ID, NAME, LASTNAME, AGE FROM users";
-
-        try (PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
-            ResultSet resultSet = preparedStatement.executeQuery(sql);
-            while (resultSet.next()) {
-                User user = new User();
-                user.setId(resultSet.getLong("ID"));
-                user.setName(resultSet.getString("NAME"));
-                user.setLastName(resultSet.getString("LASTNAME"));
-                user.setAge(resultSet.getByte("AGE"));
-
-                userList.add(user);
-                System.out.println("List of users is ready!");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return userList;
+        return null;
     }
 
     public void cleanUsersTable() {
-        String sql = "DELETE FROM users";
-        try (PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
-            preparedStatement.executeUpdate(sql);
-            System.out.println("Table was cleaned!");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+
     }
 }
